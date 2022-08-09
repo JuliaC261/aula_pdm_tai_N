f6f28a348ee7db67a61a8d95631e60fc5c9f89ce
J.J <99402892+JuliaC261@users.noreply.github.com>
2022-08-09 11:45:26 -0300

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+<head>
+	<meta charsets="utf-8">
+	<meta name="viewport" contents="width=devide-width, initial-scale=1">
+	<!--tag para site responsivo-->
+	<meta name="description" contents="Desrição do meu web site">
+	<!--para o google encontrar o site-->
+	<meta name="keywords" contents="palavras-chave,do,meu,site">
+	<title>Projeto 01</title>
+</head>
+
+<body>
+	
+	<!--iniciar a marcação do meu site-->
+	<header>
+		<div class="center">
+		<div class="logo">Logomarca</div> <!-- logo -->
+		<nav class="desktop"> <!--site será responsivo-->
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Serviços</a></li>
+				<li><a href="">Contato</a></li>
+
+			</ul>
+		</nav>
+
+		<nav class="mobile"> <!-- site será responsivo-->
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Serviços</a></li>
+				<li><a href="">Contato</a></li>
+
+			</ul>
+		</nav>
+	 </div>
+    </header>
+
+    <!-- banner -->
+    <section class="banner-principal">
+    	<div class="center">
+    	<!-- formulario onde o usuário vai colocar o email -->
+
+    	<form>
+    		<h2>Qual o seu E-Mail?</h2>
+    		<input type="email" name="e-mail" required/>
+
+
+</body>
+
+</html>
